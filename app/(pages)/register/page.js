49fbd1ab6efb929879a2a5290bd5b2f44fcba5a3
AngeloCarnevale <angelo.carnevale@hotmail.com>
2023-11-23 10:57:37 -0300

    login systen

--- app/(pages)/register/page.js
+++ app/(pages)/register/page.js
@@ -1,25 +1,38 @@
+'use client'
+
+import { useState } from "react"
+import {signIn} from 'next-auth/react'
+
+
 export default function Register() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = async () => {
+        const resposne = await signIn('credentials', {
+            redirect: false,
+            email: email,
+            password:password
+        })
+    }
     return (
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <img
-                    className="mx-auto h-10 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                    alt="Your Company"
-                />
                 <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                    Sign in to your account
+                    Register your account
                 </h2>
             </div>
 
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form className="space-y-6" action="#" method="POST">
+                <form className="space-y-6" onSubmit={handleLogin}>
                     <div>
                         <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                             Email address
                         </label>
                         <div className="mt-2">
                             <input
+                                onChange={(e) => setEmail(e.target.value)}
                                 id="email"
                                 name="email"
                                 type="email"
@@ -35,14 +48,10 @@ export default function Register() {
                             <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                                 Password
                             </label>
-                            <div className="text-sm">
-                                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                                    Forgot password?
-                                </a>
-                            </div>
                         </div>
                         <div className="mt-2">
                             <input
+                                onChange={(e) => setPassword(e.target.value)}
                                 id="password"
                                 name="password"
                                 type="password"
@@ -58,17 +67,10 @@ export default function Register() {
                             type="submit"
                             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
-                            Sign in
+                            Register
                         </button>
                     </div>
                 </form>
-
-                <p className="mt-10 text-center text-sm text-gray-500">
-                    Not a member?{' '}
-                    <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                        Start a 14 day free trial
-                    </a>
-                </p>
             </div>
         </div>
     )
---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,46 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+import axios from "axios";
+import { api } from "@/services/api";
+
+
+const authOptions = {
+    secret: process.env.NEXTAUTH_SECRET,
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                email: { label: 'Email', type: 'email' },
+                password: { label: 'Password', type: 'password' }
+            },
+            async authorize(credentials) {
+                const user = await api.post('/login', {
+                    email: credentials?.email,
+                    password: credentials?.password
+                }).then(data => { return data.data })
+
+                if (user == null) {
+                    return null
+                }
+                return user
+            }
+        })
+    ],
+    pages: {
+        signIn: '/login'
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            user && (token.user = user)
+            return token
+        },
+        async session({ session, token }) {
+            session = token.user
+            return session
+        }
+    }
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST, authOptions }
